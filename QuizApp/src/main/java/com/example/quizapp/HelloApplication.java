5874525c2aeccc61b6b0e9a30554668acc38d50d
lupog-code <gabri.lupo374@gmail.com>
2025-05-27 16:37:45 +0200

    jh

--- QuizApp/src/main/java/com/example/quizapp/HelloApplication.java
+++ QuizApp/src/main/java/com/example/quizapp/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("registrazione-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 400);
         stage.setMinHeight(400);
         stage.setMinWidth(400);
--- QuizApp/src/main/java/com/example/quizapp/MainController.java
+++ QuizApp/src/main/java/com/example/quizapp/MainController.java
@@ -21,4 +21,7 @@ public class MainController {
 
     public void toggleMode(ActionEvent actionEvent) {
     }
+
+    public void backToLogin(ActionEvent actionEvent) {
+    }
 }
