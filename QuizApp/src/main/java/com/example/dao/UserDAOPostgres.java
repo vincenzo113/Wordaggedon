277b1aca9b71f11ae41b43ea025255895cbb2cc9
lupog-code <gabri.lupo374@gmail.com>
2025-05-27 17:01:47 +0200

    h

---
+++ QuizApp/src/main/java/com/example/dao/UserDAOPostgres.java
@@ -0,0 +1,9 @@
+package com.example.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDAOPostgres<User> {
+    public static final String URL = "jdbc:postgresql://localhost:5432/quiz";
+    
+}
---
+++ QuizApp/src/main/java/com/example/dao/UserUserDAOPostgres.java
@@ -0,0 +1,7 @@
+package com.example.dao;
+
+import com.example.models.User;
+
+public class UserUserDAOPostgres implements UserDAOPostgres<User> {
+
+}
