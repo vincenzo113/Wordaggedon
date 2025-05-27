bdefab18a6c43655d72b579829e1414de1a587d7
lupog-code <gabri.lupo374@gmail.com>
2025-05-27 16:22:49 +0200

    a

--- QuizApp/src/main/java/com/example/quizapp/MainController.java
+++ QuizApp/src/main/java/com/example/quizapp/MainController.java
@@ -1,14 +1,24 @@
 package com.example.quizapp;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void handleLogin(ActionEvent actionEvent) {
+    }
+
+    public void handleRegister(ActionEvent actionEvent) {
+    }
+
+    public void toggleMode(ActionEvent actionEvent) {
     }
 }
