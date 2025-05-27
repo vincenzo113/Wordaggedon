9ea97f29f46bd597b9c4d7a1e6b86092fbf9dbd2
lupog-code <gabri.lupo374@gmail.com>
2025-05-27 16:27:32 +0200

    a

--- QuizApp/src/main/java/com/example/quizapp/HelloApplication.java
+++ QuizApp/src/main/java/com/example/quizapp/HelloApplication.java
@@ -11,7 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        stage.setMinHeight(400);
+        stage.setMinWidth(400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
