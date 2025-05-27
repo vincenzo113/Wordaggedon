84c6ff65d5cbf16adc833bed62c834baad62d45b
zolvyo <45299201+frncy01@users.noreply.github.com>
2025-05-27 17:28:58 +0200

    fix

--- QuizApp/src/main/java/com/example/dao/UserDAOPostgres.java
+++ QuizApp/src/main/java/com/example/dao/UserDAOPostgres.java
@@ -3,7 +3,12 @@ package com.example.dao;
 import java.util.List;
 import java.util.Optional;
 
-public interface UserDAOPostgres<User> {
+public interface UserDAOPostgres<T> {
     public static final String URL = "jdbc:postgresql://localhost:5432/quiz";
-    
+
+    Optional<T> select(String username);
+    List<T> selectAll();
+    void insert(T t);
+    //void update(T t);
+    void delete(T t);
 }
--- QuizApp/src/main/java/com/example/dao/UserUserDAOPostgres.java
+++ QuizApp/src/main/java/com/example/dao/UserUserDAOPostgres.java
@@ -2,6 +2,51 @@ package com.example.dao;
 
 import com.example.models.User;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 public class UserUserDAOPostgres implements UserDAOPostgres<User> {
 
+    private final String USER = "postgres";
+    private final String PASS = "tw2024";
+
+    @Override
+    public Optional<User> select(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> selectAll() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void insert(User user) {
+        try(
+                Connection c = DriverManager.getConnection(URL, USER, PASS);
+                Statement s = c.createStatement();
+        ) {
+            String insert = String.format("INSERT INTO public.user(username, password, role) VALUES ('%s', '%s', '%s')", user.getUsername(), user.getPassword(), user.getRole());
+            s.executeUpdate(insert);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(User user) {
+        try(
+                Connection c = DriverManager.getConnection(URL, USER, PASS);
+                Statement s = c.createStatement();
+        ) {
+            String delete = String.format("DELETE FROM public.user WHERE username = '%s'", user.getUsername());
+            s.executeUpdate(delete);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
