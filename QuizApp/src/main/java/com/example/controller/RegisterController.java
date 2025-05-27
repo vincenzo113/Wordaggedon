f6e5dbcb2e5e10c1b470bc2a122613f2540e1ff2
zolvyo <45299201+frncy01@users.noreply.github.com>
2025-05-27 18:24:52 +0200

    register

--- QuizApp/src/main/java/com/example/controller/RegisterController.java
+++ QuizApp/src/main/java/com/example/controller/RegisterController.java
@@ -1,4 +1,50 @@
 package com.example.controller;
 
+import com.example.dao.UserDAOPostgresImp;
+import com.example.models.User;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
+
 public class RegisterController {
+    public TextField usernameField;
+    public PasswordField passwordField;
+    public PasswordField confirmPasswordField;
+    public Button registerButton;
+    public UserDAOPostgresImp userDAOPostgres;
+
+    public RegisterController(TextField usernameField, PasswordField passwordField, PasswordField confirmPasswordField, Button registerButton) {
+        this.usernameField = usernameField;
+        this.passwordField = passwordField;
+        this.confirmPasswordField = confirmPasswordField;
+        this.registerButton = registerButton;
+    }
+
+    public void registerUser() throws SQLException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        String confirmPassword = confirmPasswordField.getText();
+
+
+        if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            System.out.println("All fields are required.");
+            return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            System.out.println("Passwords do not match.");
+            return;
+        }
+        User user = new User(username,password,"user");
+        userDAOPostgres = new UserDAOPostgresImp();
+        boolean isRegistered = userDAOPostgres.isRegistered(user);
+
+        if (isRegistered) {
+            System.out.println("User registered successfully.");
+        } else {
+            System.out.println("Registration failed. Username may already exist.");
+        }
+    }
 }
