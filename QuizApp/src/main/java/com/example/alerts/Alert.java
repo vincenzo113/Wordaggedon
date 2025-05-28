9dca8637981ed1320080a699170b18903629fe49
Vincenzo Ragone <v.ragone8@studenti.unisa.it>
2025-05-28 14:46:56 +0200

    s

--- QuizApp/src/main/java/com/example/alerts/Alert.java
+++ QuizApp/src/main/java/com/example/alerts/Alert.java
@@ -4,40 +4,39 @@ public class Alert {
 
     public static void showAlert(AlertList alertType) {
         javafx.scene.control.Alert alert = new javafx.scene.control.Alert(javafx.scene.control.Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
 
-        if(alertType == AlertList.LOGIN_SUCCESS) {
-            alert.setTitle("Login Success");
-            alert.setHeaderText(null);
-            alert.setContentText(alertType.getMessage());
-        } else if(alertType == AlertList.LOGIN_FAILURE) {
-            alert.setTitle("Login Failure");
-            alert.setHeaderText(null);
-            alert.setContentText(alertType.getMessage());
-        } else if(alertType == AlertList.REGISTER_SUCCESS) {
-            alert.setTitle("Registration Success");
-            alert.setHeaderText(null);
-            alert.setContentText(alertType.getMessage());
-        } else if(alertType == AlertList.REGISTER_FAILURE) {
-            alert.setTitle("Registration Failure");
-            alert.setHeaderText(null);
-            alert.setContentText(alertType.getMessage());
-        } else if(alertType == AlertList.FIELDS_EMPTY) {
-            alert.setTitle("Fields Empty");
-            alert.setHeaderText(null);
-            alert.setContentText(alertType.getMessage());
-        } else if(alertType == AlertList.DATABASE_ERROR) {
-            alert.setTitle("Database Error");
-            alert.setHeaderText(null);
-            alert.setContentText(alertType.getMessage());
-        } else if(alertType == AlertList.USER_NOT_FOUND) {
-            alert.setTitle("User Not Found");
-            alert.setHeaderText(null);
-            alert.setContentText(alertType.getMessage());
-        } else if(alertType == AlertList.PASSWORD_MISMATCH) {
-            alert.setTitle("Password Mismatch");
-            alert.setHeaderText(null);
-            alert.setContentText(alertType.getMessage());
+        switch (alertType) {
+            case LOGIN_SUCCESS:
+                alert.setTitle("Login avvenuto con successo");
+                break;
+            case LOGIN_FAILURE:
+                alert.setTitle("Login Fallito");
+                break;
+            case REGISTER_SUCCESS:
+                alert.setTitle("Registration avvenuta con successo");
+                break;
+            case REGISTER_FAILURE:
+                alert.setTitle("Registration fallita , riprova");
+                break;
+            case FIELDS_EMPTY:
+                alert.setTitle("Errore , campi non compilati");
+                break;
+            case DATABASE_ERROR:
+                alert.setTitle("Errore");
+                break;
+            case USER_NOT_FOUND:
+                alert.setTitle("Utente non trovato");
+                break;
+            case PASSWORD_MISMATCH:
+                alert.setTitle("Password non corrispondono");
+                break;
+            default:
+                alert.setTitle("Alert");
+                break;
         }
+
+        alert.setContentText(alertType.getMessage());
         alert.showAndWait();
     }
 }
