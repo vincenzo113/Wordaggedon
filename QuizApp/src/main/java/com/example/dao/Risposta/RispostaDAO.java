957e167c5d3e8fc397d5a677c5d95d40ed598506
lupog-code <gabri.lupo374@gmail.com>
2025-06-10 17:23:06 +0200

    fix alcuni dao

--- QuizApp/src/main/java/com/example/dao/Risposta/RispostaDAO.java
+++ QuizApp/src/main/java/com/example/dao/Risposta/RispostaDAO.java
@@ -1,6 +1,7 @@
 package com.example.dao.Risposta;
 
 import com.example.dao.ConnectionConfig;
+import com.example.exceptions.DatabaseException;
 import com.example.models.Documento;
 import com.example.models.Risposta;
 
@@ -22,8 +23,10 @@ public interface RispostaDAO extends ConnectionConfig {
      * @param documento il documento di riferimento.
      * @param parola la parola da cercare.
      * @return la risposta corretta corrispondente.
+     * @throws DatabaseException se si verifica un errore durante l'accesso al database
      */
-    Risposta selectRispostaCorrettaRipetizioneParolaDocumento(Documento documento, String parola);
+    Risposta selectRispostaCorrettaRipetizioneParolaDocumento(Documento documento, String parola) 
+            throws DatabaseException;
 
     /**
      * Recupera la risposta corretta quando non è presente alcuna corrispondenza
@@ -31,24 +34,30 @@ public interface RispostaDAO extends ConnectionConfig {
      *
      * @param documenti la lista di documenti da considerare.
      * @return la risposta corretta relativa al caso "non presente".
+     * @throws DatabaseException se si verifica un errore durante l'accesso al database
      */
-    Risposta selectRispostaCorrettaNonPresente(List<Documento> documenti);
+    Risposta selectRispostaCorrettaNonPresente(List<Documento> documenti) 
+            throws DatabaseException;
 
     /**
      * Recupera la risposta corretta più frequente tra tutti i documenti forniti.
      *
      * @param documenti la lista di documenti da analizzare.
      * @return la risposta corretta più frequente in tutti i documenti.
+     * @throws DatabaseException se si verifica un errore durante l'accesso al database
      */
-    Risposta selectRispostaCorrettaPiuFrequenteInTutti(List<Documento> documenti);
+    Risposta selectRispostaCorrettaPiuFrequenteInTutti(List<Documento> documenti) 
+            throws DatabaseException;
 
     /**
      * Recupera la risposta corretta più frequente all'interno di un singolo documento.
      *
      * @param documento il documento da analizzare.
      * @return la risposta corretta più frequente nel documento.
+     * @throws DatabaseException se si verifica un errore durante l'accesso al database
      */
-    Risposta selectRispostaCorrettaPiuFrequenteDocumento(Documento documento);
+    Risposta selectRispostaCorrettaPiuFrequenteDocumento(Documento documento) 
+            throws DatabaseException;
 
     /**
      * Recupera una lista di risposte associate a parole che non sono più frequenti
@@ -57,8 +66,10 @@ public interface RispostaDAO extends ConnectionConfig {
      * @param documento il documento di riferimento.
      * @param parola la parola di riferimento.
      * @return lista di risposte relative a parole meno frequenti.
+     * @throws DatabaseException se si verifica un errore durante l'accesso al database
      */
-    List<Risposta> selectParoleNonPiuFrequenti(Documento documento, String parola);
+    List<Risposta> selectParoleNonPiuFrequenti(Documento documento, String parola) 
+            throws DatabaseException;
 
     /**
      * Recupera una lista di risposte associate a parole che non sono più frequenti
@@ -67,8 +78,10 @@ public interface RispostaDAO extends ConnectionConfig {
      * @param documenti la lista di documenti da considerare.
      * @param parolaCorretta la parola corretta di riferimento.
      * @return lista di risposte relative a parole meno frequenti in tutti i documenti.
+     * @throws DatabaseException se si verifica un errore durante l'accesso al database
      */
-    List<Risposta> selectParoleNonPiuFrequentiInTutti(List<Documento> documenti, String parolaCorretta);
+    List<Risposta> selectParoleNonPiuFrequentiInTutti(List<Documento> documenti, String parolaCorretta) 
+            throws DatabaseException;
 
     /**
      * Recupera una lista di risposte associate a parole che sono presenti
@@ -76,7 +89,8 @@ public interface RispostaDAO extends ConnectionConfig {
      *
      * @param docs la lista di documenti da analizzare.
      * @return lista di risposte relative a parole presenti in almeno un documento.
+     * @throws DatabaseException se si verifica un errore durante l'accesso al database
      */
-    List<Risposta> selectParolePresentiInAlmenoUnDocumento(List<Documento> docs);
-
+    List<Risposta> selectParolePresentiInAlmenoUnDocumento(List<Documento> docs) 
+            throws DatabaseException;
 }
