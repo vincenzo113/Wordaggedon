7150f0248fd7124322bf7fab4bc815721a3f1fe8
Vincenzo Ragone <v.ragone8@studenti.unisa.it>
2025-06-10 11:56:39 +0200

    Limite = 3

--- QuizApp/src/main/java/com/example/dao/Documento/DocumentoDAO.java
+++ QuizApp/src/main/java/com/example/dao/Documento/DocumentoDAO.java
@@ -8,10 +8,36 @@ import com.example.models.Documento;
 import java.sql.SQLException;
 import java.util.List;
 
+/**
+ * Interfaccia per la gestione dei documenti in un database, con metodi
+ * per l'inserimento, il recupero e la selezione basata sulla difficoltà.
+ *
+ * @param <T> il tipo di oggetto Documento gestito
+ */
 public interface DocumentoDAO<T> extends ConnectionConfig {
 
+    /**
+     * Restituisce una lista di documenti in base alla difficoltà specificata.
+     *
+     * @param difficolta la difficoltà selezionata dall'utente
+     * @return lista di documenti corrispondenti alla difficoltà
+     * @throws NotEnoughDocuments se non ci sono abbastanza documenti per la difficoltà richiesta
+     */
     public List<T> getDocumentiPerDifficolta(DifficultyEnum difficolta) throws NotEnoughDocuments;
+
+    /**
+     * Inserisce un nuovo documento nel database.
+     *
+     * @param documento il documento da inserire
+     * @throws SQLException se si verifica un errore durante l'inserimento
+     */
     public void insertDocumento(Documento documento) throws SQLException;
+
+    /**
+     * Recupera tutti i documenti presenti nel database.
+     *
+     * @return lista di tutti i documenti
+     */
     public List<Documento> getAllDocuments();
 
 }
