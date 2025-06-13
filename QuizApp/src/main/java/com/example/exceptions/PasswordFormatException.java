9f512730f1b26c8d5e76fe4d4807c62d058c75f7
lupog-code <gabri.lupo374@gmail.com>
2025-06-13 11:55:14 +0200

    alert

--- QuizApp/src/main/java/com/example/exceptions/PasswordFormatException.java
+++ QuizApp/src/main/java/com/example/exceptions/PasswordFormatException.java
@@ -1,6 +1,15 @@
 package com.example.exceptions;
 
+/**
+ * Eccezione lanciata quando una password non soddisfa i criteri di formato richiesti.
+ */
 public class PasswordFormatException extends RuntimeException {
+    
+    /**
+     * Costruisce una nuova PasswordFormatException con il messaggio di dettaglio specificato.
+     * 
+     * @param message il messaggio di dettaglio che spiega perché il formato della password non è valido
+     */
     public PasswordFormatException(String message) {
         super(message);
     }
